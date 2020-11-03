a63569d15344e2298c2006a135192e76f3bdf5ff
jorge tapicha <jorge.tapicha@rappi.com>
2020-11-02 21:40:45 -0500

    initialize project

---
+++ main.tf
@@ -0,0 +1,13 @@
+locals {
+    env = terraform.workspace
+    common_tags = {
+       application = "eks-gke-k8s-multicluster"
+       env = local.env
+    }
+}
+
+provider "aws" {
+  version = "~> 3.0"
+  region  = var.aws_region
+  profile = var.aws_profile
+}
